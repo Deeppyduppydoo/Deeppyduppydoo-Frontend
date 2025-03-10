5a7978ed277103ac83ade0608aeb72e0e5b46860
mewpk <milzaza123@gmail.com>
2025-03-10 19:50:53 +0700

    Add Dockerfile for application containerization and enhance UploadFile component styling

--- src/app/components/UploadFile.tsx
+++ src/app/components/UploadFile.tsx
@@ -116,9 +116,8 @@ const UploadFile: React.FC<UploadFileProps> = ({ setStep, setFile, setSelectedMo
 
 
   return (
-    <div className="p-8 px-20 bg-white shadow-lg rounded-xl w-full max-w-3xl text-center">
+    <div className="p-8 px-20 bg-white shadow-2xl rounded-xl w-full max-w-3xl text-center ">
       <h2 className="text-2xl font-bold text-gray-700 mb-6">Draw Something</h2>
-    
 
       <div className="mt-6">
         <div
@@ -142,7 +141,7 @@ const UploadFile: React.FC<UploadFileProps> = ({ setStep, setFile, setSelectedMo
         <div className="mt-4 flex justify-center gap-4">
           <button
             onClick={clearCanvas}
-            className="px-4 py-2 bg-gray-400 text-white rounded-lg hover:bg-gray-500"
+            className="px-4 py-2 w-full bg-gray-400 text-white rounded-lg hover:bg-gray-500"
           >
             Clear
           </button>
@@ -166,7 +165,7 @@ const UploadFile: React.FC<UploadFileProps> = ({ setStep, setFile, setSelectedMo
       {/* Upload Button */}
       <button
         onClick={handleUpload}
-        className="mt-6 px-6 py-3 font-medium text-white rounded-lg shadow-md transition duration-300 bg-blue-500 hover:bg-blue-600"
+        className="mt-6 px-6 py-3 w-full font-medium text-white rounded-lg shadow-md transition duration-300 bg-blue-500 hover:bg-blue-600"
       >
         Upload
       </button>
