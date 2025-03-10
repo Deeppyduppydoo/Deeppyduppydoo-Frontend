195c11bc3fe5338d074bea81b9ac97fe01bb7ba1
mewpk <milzaza123@gmail.com>
2025-03-10 20:05:07 +0700

    Add new profile images and enhance Success component styling

--- src/app/components/Success.tsx
+++ src/app/components/Success.tsx
@@ -9,9 +9,8 @@ interface SuccessProps {
 
 const Success: React.FC<SuccessProps> = ({ setStep, responseData }) => {
   return (
-    <div className="p-6 bg-white shadow-lg rounded-md w-full max-w-md text-center">
-      <h2 className="text-lg font-semibold text-green-500">Success!</h2>
-      <p className="mt-2 text-gray-600">Your file has been successfully uploaded.</p>
+    <div className="p-6 bg-white shadow-lg rounded-md w-full min-w-2xl max-w-md text-center flex flex-col gap-10">
+      <h2 className="text-5xl font-semibold text-green-500">Success!</h2>
 
       {responseData && (
         <div className="mt-4 p-4 border rounded-md bg-gray-50">
